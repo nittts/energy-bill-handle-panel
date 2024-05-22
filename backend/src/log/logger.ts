f5da5d080d6e34a239d8d8f85c6ad7f6d4cfaeb9
william baierle <william.baierle@voluti.com.br>
2024-05-21 21:50:54 -0300

    feat: includes loggers with pino and multer as a file handler middleware

---
+++ backend/src/log/logger.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import pino from "pino";
+
+const pathToLog = `${path.resolve(String(process.env.BASE_UPLOAD_URL)) || "./"}/logs`;
+
+const transport = pino.transport({
+  targets: [
+    { level: "trace", target: "pino-pretty", options: {} },
+    {
+      level: "err",
+      target: "pino/file",
+      options: {
+        destination: `${pathToLog}/err`,
+        mkdir: true,
+      },
+    },
+    {
+      level: "fatal",
+      target: "pino/file",
+      options: {
+        destination: `${pathToLog}/fatal`,
+        mkdir: true,
+      },
+    },
+    {
+      level: "warn",
+      target: "pino/file",
+      options: {
+        destination: `${pathToLog}/warn`,
+        mkdir: true,
+      },
+    },
+  ],
+});
+
+const log = pino({}, transport);
+
+export { log };
---
+++ backend/src/middlewares/multer.middleware.ts
@@ -0,0 +1,60 @@
+import { config } from "dotenv";
+config();
+import { NextFunction, Request, Response } from "express";
+import multer from "multer";
+import fs from "fs";
+import { AppError } from "./asyncErrors.middleware";
+import { v4 as uuid } from "uuid";
+import path from "path";
+import { log } from "../log/logger";
+
+const maxSize = 12 * 1024 * 1024;
+
+const destinationFiles = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: (error: Error | null, destination: string) => void
+) => {
+  const baseDir = process.env.BASE_UPLOAD_URL || path.resolve(process.env.BASE_UPLOAD_URL || "");
+
+  const savePath = `${baseDir}/temp`;
+
+  try {
+    if (!fs.existsSync(savePath)) {
+      fs.mkdirSync(savePath, { recursive: true });
+    }
+  } catch (err) {
+    if (err instanceof AppError) {
+      log.error(
+        {
+          error: err.message,
+          status: 503,
+          source: "MULTER MIDDLEWARE | destination files",
+        },
+        "uncaught file destination creation."
+      );
+      throw new AppError(err.message, 503);
+    }
+  }
+
+  cb(null, savePath);
+};
+
+const filename = (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
+  const id = uuid();
+
+  cb(null, `${id}.${file.mimetype}`);
+};
+
+const files = multer.diskStorage({
+  destination: destinationFiles,
+  filename,
+});
+
+const filesStorage = multer({ storage: files, limits: { fileSize: maxSize } });
+
+async function fileUpload(req: Request, res: Response, next: NextFunction) {
+  filesStorage.array("files", 20)(req, res, next);
+}
+
+export default fileUpload;
