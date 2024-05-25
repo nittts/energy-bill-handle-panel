9410918ae8eb9a72b856f9dee19cd9e4062b6ae3
william baierle <william.baierle@voluti.com.br>
2024-05-25 14:26:35 -0300

    feat: includes delete on grid cards

--- frontend/src/components/Card/Card.actions.tsx
+++ frontend/src/components/Card/Card.actions.tsx
@@ -1,9 +1,12 @@
 import DescriptiveIcon from "@/components/DescriptiveIcon";
+import { useDeleteBill } from "@/hooks/bills";
+import { IoMdTrash } from "react-icons/io";
 import { MdReadMore } from "react-icons/md";
 import { useSearchParams } from "react-router-dom";
 
 function CardActions(id: string) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { deleteBills } = useDeleteBill();
 
   const onClick = () => {
     if (searchParams.get("billId")) return setSearchParams({}, { replace: true });
@@ -11,8 +14,11 @@ function CardActions(id: string) {
     setSearchParams({ billId: id }, { replace: true });
   };
 
+  const onDelete = () => deleteBills(id);
+
   const actions = [
     <DescriptiveIcon type="primary" title="Ver Fatura" icon={<MdReadMore size={20} />} onClick={onClick} />,
+    <DescriptiveIcon type="primary" danger title="Deletar Fatura" icon={<IoMdTrash size={20} />} onClick={onDelete} />,
   ];
 
   return actions;
